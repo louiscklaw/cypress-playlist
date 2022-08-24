6e57b04eb594786a9143e558e886619dca30eaa7
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-25 00:31:19 +0800

    update,

--- cypress-mochawesome-reporter-tryout/cypress.config.js
+++ cypress-mochawesome-reporter-tryout/cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
   e2e: {
-    setupNodeEvents(on, config) {},
     supportFile: false,
   },
 })
