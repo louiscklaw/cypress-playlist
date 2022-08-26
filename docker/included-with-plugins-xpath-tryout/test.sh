01e07015fc6293c2a6392ca2ecd573aa62628455
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-27 00:15:00 +0800

    update and test,

--- docker/included-with-plugins-xpath-tryout/test.sh
+++ docker/included-with-plugins-xpath-tryout/test.sh
@@ -8,4 +8,4 @@ echo "cd src; npm install"
 
 # print the docker command before running
 set -x
-docker run -it -v $PWD/src:/test -w /test cypress/included:3.8.0
+docker run -i -v $PWD/src:/test -w /test cypress/included:3.8.0
