618515c2dc54ee026d44040c24b5eeed11a5d7d6
louiscklaw <louiscklaw@gmail.com>
2022-08-27 23:18:45 +0800

    update and test,

--- docker/cypress-docker-v10-xpath-tryout/cypress/e2e/carousell.cy.js
+++ docker/cypress-docker-v10-xpath-tryout/cypress/e2e/carousell.cy.js
@@ -1,24 +1,31 @@
 import { onlyOn, skipOn } from '@cypress/skip-test';
 
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
+
 describe('test xpath', { retries: { runMode: 3, openMode: 1 } }, () => {
   it('helloworld', () => {
     cy.visit('https://www.carousell.com.hk');
-    cy.viewport(1920, 1080 * 10);
+    cy.viewport(1920, 1080 * 1);
 
-    cy.xpath('(.//input[@placeholder="Search for an item"])[1]').type('3D 代客打印');
-
-    cy.intercept('/search/*').as('searchText');
+    // #root > div > div > div.D_aiF.D_aiJ > div > div.D_jk > div.D_jm > div > form > div.D_or > div > div > div > input
+    cy.get('#root').get('input[placeholder="Search for an item"]').type('3D 代客打印');
     cy.xpath('(.//button[@data-testid="navbar-search-input-location-desktop-btn-search"])[1]').click();
-    cy.wait('@searchText').then(interception => {
-      // we can now access the low level interception
-      // that contains the request body,
-      // response body, status, etc
-      cy.log('search done');
-    });
 
-    // #root > div > div > div.D_aiF.D_aiJ > div > div.D__Z > div > div > div.D_Ae > a
-    cy.get('main').then($el => {
-      cy.log($el.text());
+    // // #root > div > div > div.D_aiF.D_aiJ > div > div.D__Z > div > div > div.D_Ae > a
+
+    let seller_list = cy.xpath(`(.//div[(@data-testid!="listing-card") and starts-with(@data-testid,"listing-card")])`, { timeout: 30 * 1000 });
+    seller_list.contains('louiscklaw');
+
+    // found with position
+    seller_list.each(($el, index, $list) => {
+      cy.debug(JSON.stringify({ index, text: $el.text() }));
+      if ($el.text().search(/louiscklaw/) > 0) {
+        expect(index).to.be.below(8);
+      }
     });
 
     // cy.screenshot();
