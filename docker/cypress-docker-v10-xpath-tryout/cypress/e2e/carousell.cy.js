1abed74860666421f1567dfae4abb0eecaadc6ca
louiscklaw <louiscklaw@gmail.com>
2022-08-27 19:57:56 +0800

    update and test,

--- docker/cypress-docker-v10-xpath-tryout/cypress/e2e/carousell.cy.js
+++ docker/cypress-docker-v10-xpath-tryout/cypress/e2e/carousell.cy.js
@@ -42,7 +42,7 @@ describe('test xpath', () => {
     cy.viewport(1920, 1080 * 10);
     cy.log('helloworld done');
 
-    cy.xpath('.//*[@id="root"]');
+    cy.xpath('.//div');
 
     cy.screenshot();
   });
