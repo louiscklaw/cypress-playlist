84d3b011372335d87d638ea7c85f3bf97f77c11f
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-27 15:40:43 +0800

    update,

--- docker/included-with-plugins-xpath-tryout/src/cypress/e2e/carousell-helloworld.cy.js
+++ docker/included-with-plugins-xpath-tryout/src/cypress/e2e/carousell-helloworld.cy.js
@@ -1,7 +1,29 @@
-describe('carousell', () => {
+context('carousell', () => {
+  beforeEach(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+  });
+
   it('helloworld ', { retries: 3 }, () => {
     cy.visit('https://www.carousell.com.hk');
+    cy.viewport(1920, 1080 * 10);
+    cy.get('select', { timeout: 10000 }).should('be.visible');
+    // cy.wait(5000);
+
     cy.log('helloworld');
+
     cy.screenshot();
   });
+
+  afterEach(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+  });
 });
+
+// context('Actions', () => {
+//   it('helloworld cypress', () => {
+//     cy.visit('http://www.example.com');
+//     cy.log('helloworld');
+//   });
+// });
