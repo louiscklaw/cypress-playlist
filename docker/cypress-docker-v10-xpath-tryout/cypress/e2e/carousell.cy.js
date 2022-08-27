54fa33b731803311de2d81e6843a3ae77a93879a
louiscklaw <louiscklaw@gmail.com>
2022-08-27 20:27:51 +0800

    update and test,

--- docker/cypress-docker-v10-xpath-tryout/cypress/e2e/carousell.cy.js
+++ docker/cypress-docker-v10-xpath-tryout/cypress/e2e/carousell.cy.js
@@ -3,15 +3,9 @@ import { onlyOn, skipOn } from '@cypress/skip-test';
 describe('test xpath', () => {
   it('helloworld', () => {
     cy.visit('http://www.carousell.com.hk');
-    cy.viewport(1920, 1080 * 10);
+
     cy.log('helloworld done');
 
-    cy.xpath('.//div');
-
     cy.screenshot();
-
-    cy.clearCookies();
-    cy.clearLocalStorage();
-    cy.wait(3000);
   });
 });
