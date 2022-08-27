ca760b3089cda21bc4b3858ab259e2286102d20f
louiscklaw <louiscklaw@gmail.com>
2022-08-27 19:49:56 +0800

    update and test,

--- docker/cypress-docker-v10-xpath-tryout/cypress/e2e/carousell.cy.js
+++ docker/cypress-docker-v10-xpath-tryout/cypress/e2e/carousell.cy.js
@@ -1,6 +1,40 @@
 import { onlyOn, skipOn } from '@cypress/skip-test';
 
+before(() => {
+  // root-level hook
+  // runs once before all tests
+});
+
+beforeEach(() => {
+  // root-level hook
+  // runs before every test block
+});
+
+afterEach(() => {
+  // runs after each test block
+});
+
+after(() => {
+  // runs once all tests are done
+});
+
 describe('test xpath', () => {
+  before(() => {
+    // runs once before all tests in the block
+  });
+
+  beforeEach(() => {
+    // runs before each test in the block
+  });
+
+  afterEach(() => {
+    // runs after each test in the block
+  });
+
+  after(() => {
+    // runs once after all tests in the block
+  });
+
   it('helloworld', () => {
     cy.visit('http://www.carousell.com.hk');
     cy.viewport(1920, 1080 * 10);
