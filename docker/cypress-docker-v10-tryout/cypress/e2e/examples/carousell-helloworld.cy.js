464fbb7dc98128c0a31639caef85631ba86f0368
louiscklaw <louiscklaw@gmail.com>
2022-09-08 17:29:21 +0800

    update and test,

--- docker/cypress-docker-v10-tryout/cypress/e2e/examples/carousell-helloworld.cy.js
+++ docker/cypress-docker-v10-tryout/cypress/e2e/examples/carousell-helloworld.cy.js
@@ -1,10 +1,10 @@
 context('Actions', () => {
-  beforeEach(() => {
-    cy.intercept('https://sentry.io/*', {});
-    cy.visit('https://www.carousell.com.hk');
-  });
+  beforeEach(() => {});
 
   it('helloworld carousell', () => {
+    cy.intercept('https://sentry.io/*', {});
+    cy.visit('https://www.carousell.com.hk');
+
     cy.viewport(1920, 1080 * 10);
 
     cy.screenshot({ capture: 'viewport', overwrite: true });
