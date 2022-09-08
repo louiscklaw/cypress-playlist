3c61300a03a94a274daff8f60151a990fe2d87c1
louiscklaw <louiscklaw@gmail.com>
2022-09-08 16:22:05 +0800

    update,

--- docker/cypress-docker-v10-xpath-tryout/cypress/e2e/spec.cy.js
+++ docker/cypress-docker-v10-xpath-tryout/cypress/e2e/spec.cy.js
@@ -1,26 +1,6 @@
-import { onlyOn, skipOn } from '@cypress/skip-test'
-
-onlyOn('linux', () => {
-  describe('linux tests', () => {
-    it('has h2', () => {
-      cy.visit('index.html')
-      cy.contains('h2', 'test')
-    })
-  })
-})
-
-skipOn('linux', () => {
-  describe('Tests on Mac or Windows', () => {
-    it('has h2', () => {
-      cy.visit('index.html')
-      cy.contains('h2', 'test')
-    })
-  })
-})
-
 describe('test xpath', () => {
   it('helloworld', () => {
-    cy.visit('index.html')
-    cy.xpath('.//h2')
-  })
-})
+    cy.visit('stub/index.html');
+    cy.xpath('.//h2');
+  });
+});
